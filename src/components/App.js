5091cbec8f24aa05048dcdcf647a008412428f1d
paschalidi <paschalidi@outlook.com>
2018-09-28 16:13:46 +0200

    master: *cleans * imports css from semantic

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,8 @@
-import "./App.css";
-import reactSvg from "../../public/react.svg";
 import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-heading App-flex">
-          <h2>
-            Welcome to <span className="App-react">React</span>
-          </h2>
-        </div>
-        <div className="App-instructions App-flex">
-          <img className="App-logo" src={reactSvg} />
-          <p>
-            Edit <code>src/App.js</code> and save to hot reload your changes.
-          </p>
-        </div>
-      </div>
-    );
+    return <div>this is the app</div>;
   }
 }
 
