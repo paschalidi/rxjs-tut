97cc4de16d444d10e0a14e9ab566c80fd9d4f2e4
paschalidi <paschalidi@outlook.com>
2018-09-30 16:47:54 +0200

    master: finishes searchbar ;)

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,38 +1,78 @@
 import React from "react";
 import Rx from "rxjs/Rx";
 import { rxConnect, ofActions } from "rx-connect";
-import { Input, Grid, Button } from "semantic-ui-react";
+import {
+  Input,
+  Grid,
+  Button,
+  Header,
+  Segment,
+  Image,
+  Message
+} from "semantic-ui-react";
+
+const API = "https://api.github.com/users/";
+const errors = {
+  0: "The Id you are looking for is not existent. We apologise.",
+  1: "Please provide numbers"
+};
 
 @rxConnect(() => {
   const actions = {
-    click$: new Rx.Subject()
+    type$: new Rx.Subject()
   };
 
   return [
     Rx.Observable::ofActions(actions),
-    actions.click$
-      .switchMap(() => Rx.Observable.of({ counter$: 10 }))
-      .startWith({ counter$: 1 })
+    actions.type$.flatMap(text => {
+      if (text[0].length)
+        return Rx.Observable.ajax
+          .getJSON(`${API}${text}`)
+          .switchMap(res => Rx.Observable.of({ data: res, error: {} }))
+          .catch(() => Rx.Observable.of({ error: { message: errors[0] } }));
+
+      return Rx.Observable.of({ data: {} });
+    })
   ];
 })
 class SearchBar extends React.Component {
   render() {
-    const { click, counter$ } = this.props;
-    console.log(this.props);
-
+    const { type, error, data } = this.props;
     return (
       <Grid>
-        <Grid.Row centered columns={3} style={{ paddingTop: 200 }}>
+        <Grid.Row centered columns={2} style={{ paddingTop: 50 }}>
           <Grid.Column>
-            <h1>Search for github Users</h1>
-            <div>Type username here:</div>
-            <Input placeholder="Search..." fluid style={{ paddingBottom: 2 }} />
-            <Button fluid primary onClick={() => click()}>
-              Search
-            </Button>
-            <div>{counter$}</div>
+            <Header as="h1">Search for in database</Header>
+            <div style={{ paddingBottom: 3 }}>Type album ID:</div>
+            <Input
+              placeholder="Search..."
+              focus
+              fluid
+              onChange={e => type(e.target.value)}
+            />
+
+            {error &&
+              error.message && <Message color="red">{error.message}</Message>}
           </Grid.Column>
         </Grid.Row>
+        {data &&
+          data.id &&
+          !error.message && (
+            <Grid.Row centered columns={2}>
+              <Grid.Column>
+                <Segment>
+                  <Header as="h2">{data.login}</Header>
+                  <Header as="h4">
+                    link:{" "}
+                    <a href={data.url} target="_blank">
+                      {data.url}
+                    </a>
+                  </Header>
+                  <Image fluid centered src={data.avatar_url} />
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          )}
       </Grid>
     );
   }
