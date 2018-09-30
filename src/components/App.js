2ce29b2c344702377c742b09efc5aaba67283635
paschalidi <paschalidi@outlook.com>
2018-09-30 13:31:52 +0200

    master: edits App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
+import SearchBar from "./SearchBar";
 
-class App extends Component {
-  render() {
-    return <div>this is the app</div>;
-  }
-}
+const App = () => (
+    <SearchBar />
+);
 
 export default App;
